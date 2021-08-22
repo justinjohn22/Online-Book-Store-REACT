1a52c6a89990545ba2caeb407fab661ad6b92577
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-08-23 00:36:38 +1000

    added image to landing page, fixed css

--- FrontEnd/myfirstapp/src/components/Layout/Landing.js
+++ FrontEnd/myfirstapp/src/components/Layout/Landing.js
@@ -16,26 +16,28 @@ class Landing extends Component {
   render() {
     return (
       <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div class="grey-container">
-                          <div className="main-text">
-                            <h1><b>Welcome to BOOKEROO! </b></h1>  
-                            <h4>(the world’s best book selling/sharing service)</h4>
-                            <br />
-                            <h5><b>Which option suits you?</b></h5>
-                            <button onClick={clickMe}>
-                              View Books
-                            </button>
-                            &nbsp; &nbsp; &nbsp; 
-                            <button onClick={clickMe1}>
-                              Sell Books
-                            </button>
-                          </div> 
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <div className="row">
+              <div className="col-md-12">
+                  <div class="grey-container">
+                      <div className="main-text">
+                        <br />
+                        <h1><b>Welcome to BOOKEROO! </b></h1>  
+                        <h4>(the world’s best book selling/sharing service)</h4>
+                        <br />
+                        <h5><b>Which option suits you?</b></h5>
+                        <br />
+                        <button onClick={clickMe}>
+                          <b> VIEW BOOKS </b>
+                        </button>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                        <button onClick={clickMe1}>
+                        <b> SELL BOOKS </b>
+                        </button>                       
+                      </div>         
+                  </div>
+              </div>
+          </div>
+      </div>
     );
   }
 }

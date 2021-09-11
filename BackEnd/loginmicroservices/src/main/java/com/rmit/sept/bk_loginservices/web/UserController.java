0f0895e30cfeb32e5a999ffd42459629fa537c92
s3842912 <s3842912@student.rmit.edu.au>
2021-09-11 18:49:05 +1000

    Add API endpoint to get all users

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/web/UserController.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/web/UserController.java
@@ -1,9 +1,9 @@
 package com.rmit.sept.bk_loginservices.web;
 
-
 import com.rmit.sept.bk_loginservices.model.User;
 import com.rmit.sept.bk_loginservices.payload.JWTLoginSucessReponse;
 import com.rmit.sept.bk_loginservices.payload.LoginRequest;
+import com.rmit.sept.bk_loginservices.repositories.UserRepository;
 import com.rmit.sept.bk_loginservices.security.JwtTokenProvider;
 import com.rmit.sept.bk_loginservices.services.MapValidationErrorService;
 import com.rmit.sept.bk_loginservices.services.UserService;
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +28,11 @@ import static com.rmit.sept.bk_loginservices.security.SecurityConstant.TOKEN_PRE
 
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private MapValidationErrorService mapValidationErrorService;
@@ -38,6 +42,13 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    
+    // Get all users in the database
+    @GetMapping("/all")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
 
     @PostMapping("/register")

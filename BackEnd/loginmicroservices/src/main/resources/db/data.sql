2ec075b3ae12eca55f6675762bb3baeeac7ae1b4
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-09-19 21:01:23 +1000

    initial styling with dummy books

--- BackEnd/loginmicroservices/src/main/resources/db/data.sql
+++ BackEnd/loginmicroservices/src/main/resources/db/data.sql
@@ -1,8 +1,93 @@
 INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
-VALUES ('Justin',  'Book 1', 29, 'link', 'sample text', '123123ASD', '2020-04-06', '2020-03-08');
+VALUES ('Charles Dickens',  'A Tale of Two Cities', 25, 
+        'https://drive.google.com/uc?id=1pbaT5c_-UnuPdpkBIPwaYg8W5-EokLDc', 
+        'The novel tells the story of the French Doctor Manette...', 
+        '9780140430547', '2021-09-19', '2021-09-19');
 
 INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
-VALUES ('Vince',  'Book 1', 29, 'link', 'sample text', '123123ASD', '2020-04-06', '2020-03-08');
+VALUES ('Andy Weir',  'The Martian', 30, 
+        'https://drive.google.com/uc?id=1r3o3ILJhqhz-E-c5Fya-3qOffyApZSfH', 
+        'The story follows an American astronaut, Mark Watney...', 
+        '678131142330542', '2021-09-19', '2021-09-19');
 
 INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
-VALUES ( 'asd',  'Book 1', 29, 'link', 'sample text', '123123ASD', '2020-04-06', '2020-03-08');
+VALUES ('Sofi Oksanen',  'When the Doves Disappeared', 35, 
+        'https://drive.google.com/uc?id=1k1-lZHFvjxWcoayuIt55RjbtfKRISZM-', 
+        'Communist Estonia, 1941. As the Allies and the Axis clash in battle...', 
+        '878131322330123', '2021-09-19', '2021-09-19');
+
+INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+VALUES ('Nora Barrett',  'The King of Drugs', 15, 
+        'https://drive.google.com/uc?id=1wTA8XG9e0A8M4fveCLktsACJB0IFHl-C', 
+        'The story of notorious drug kingpin Pablo Escobar, who became....', 
+        '993231322330123', '2021-09-19', '2021-09-19');
+
+INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+VALUES ('Kelly McWilliams',  'Agnes at the End of the World', 25, 
+        'https://drive.google.com/uc?id=1yRPLL4zmK4rHYy5UZXs7gu0Vkd3H30wX', 
+        'The Handmaids Tale meets Wilder Girls in this genre-defying novel...', 
+        '876431322330121', '2021-09-19', '2021-09-19');
+
+INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+VALUES ('Charles Dickens',  'A Tale of Two Cities', 25, 
+        'https://drive.google.com/uc?id=1pbaT5c_-UnuPdpkBIPwaYg8W5-EokLDc', 
+        'The novel tells the story of the French Doctor Manette...', 
+        '123123ASD', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Charles Dickens',  'A Tale of Two Cities', 25, 
+--         'https://drive.google.com/file/d/1pbaT5c_-UnuPdpkBIPwaYg8W5-EokLDc/view', 
+--         'The novel tells the story of the French Doctor Manette, 
+--         his 18-year-long imprisonment in the Bastille in Paris 
+--         and his release to live in London with his daughter Lucie,
+--         whom he had never met. The story is set against the conditions 
+--         that led up to the French Revolution and the Reign of Terror.', 
+--         '9780140430547', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Andy Weir',  'The Martian', 30, 
+--         'https://drive.google.com/file/d/1r3o3ILJhqhz-E-c5Fya-3qOffyApZSfH/view', 
+--         'The story follows an American astronaut, Mark Watney, 
+--         as he becomes stranded alone on Mars in 2035 and must 
+--         improvise in order to survive. The Martian, a film 
+--         adaptation directed by Ridley Scott and starring Matt
+--         Damon, was released in October 2015.', 
+--         '678131142330542', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Sofi Oksanen',  'When the Doves Disappeared', 35, 
+--         'https://drive.google.com/file/d/1k1-lZHFvjxWcoayuIt55RjbtfKRISZM-/view', 
+--         'Communist Estonia, 1941. As the Allies and the Axis 
+--         clash in battle, two Estonian cousins are fleeing from 
+--         the Red Army: Roland, a loyal freedom fighter, and Edgar, 
+--         an opportunistic mercenary. When the Nazis take control 
+--         of the country, Roland goes into hiding.', 
+--         '878131322330123', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Nora Barrett',  'The King of Drugs', 15, 
+--         'https://drive.google.com/file/d/1wTA8XG9e0A8M4fveCLktsACJB0IFHl-C/view', 
+--         'The story of notorious drug kingpin Pablo Escobar, who became a billionaire 
+--         through the production and distribution of cocaine, while also focusing on 
+--         Escobars interactions with other drug lords, DEA agents, and various 
+--         opposition entities.', 
+--         '993231322330123', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Kelly McWilliams',  'Agnes at the End of the World', 25, 
+--         'https://drive.google.com/file/d/1yRPLL4zmK4rHYy5UZXs7gu0Vkd3H30wX/view', 
+--         'The Handmaids Tale meets Wilder Girls in this genre-defying novel about 
+--         a girl who escapes a terrifying cult only to discover that the world 
+--         Outside has succumbed to a viral apocalypse. Agnes loves her home of Red 
+--         Creek—its quiet, sunny mornings, its dusty roads, and its God.', 
+--         '876431322330121', '2021-09-19', '2021-09-19');
+
+-- INSERT INTO books ( author, name, cost, cover_image, description, isbn, create_at, update_at)
+-- VALUES ('Charles Dickens',  'A Tale of Two Cities', 25, 
+--         'https://drive.google.com/file/d/1pbaT5c_-UnuPdpkBIPwaYg8W5-EokLDc/view', 
+--         'The novel tells the story of the French Doctor Manette, 
+--         his 18-year-long imprisonment in the Bastille in Paris 
+--         and his release to live in London with his daughter Lucie,
+--         whom he had never met. The story is set against the conditions 
+--         that led up to the French Revolution and the Reign of Terror.', 
+--         '123123ASD', '2021-09-19', '2021-09-19');
--- FrontEnd/myfirstapp/src/components/Books/AllBooks.js
+++ FrontEnd/myfirstapp/src/components/Books/AllBooks.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 import '../styles/AllBooks.css'
 
 class AllBooks extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    // Get the list of books
+    axios.get("http://localhost:8080/api/v1/books/all")
+    .then(res => {
+      const books = res.data;
+      this.setState({ books })
+    })
+  }
+
   render() {
     return (
       <div className="container-books">
@@ -12,7 +26,24 @@ class AllBooks extends Component {
             <h5 className="slab-serif-white">filter | 0 items</h5>
           </div>
           <div className="books-container">
-            
+            {
+              this.state.books.map(book => {
+                return (        
+                  <p> 
+                    <div className="image-group">
+                    <img className="cover-image"
+                    src={book.coverImage}
+                    alt="new"
+                    />
+                    <br></br>
+                    <b>
+                      {book.bookName}
+                    </b>
+                    </div>
+                  </p> 
+                )
+              })
+            }
           </div>
         </div>
       </div>

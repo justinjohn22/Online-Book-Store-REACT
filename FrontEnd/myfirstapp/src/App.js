4da188bac9d1e2b9536db9925d39fbf432ead81e
s3842912 <s3842912@student.rmit.edu.au>
2021-10-01 20:15:23 +1000

    Remove unused pages originating from base code

--- FrontEnd/myfirstapp/src/App.js
+++ FrontEnd/myfirstapp/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer.js";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddPerson from "./components/Persons/AddPerson";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -15,9 +13,6 @@ import PendingBusinessApps from "./components/Administration/PendingBusinessApps
 import Landing from "./components/Layout/Landing";
 import AboutUs from "./components/StaticPages/AboutUs";
 
-
-// import Register from "./components/UserManagement/Register";
-// import Login from "./components/UserManagement/Login";
 import LoginPage from "./components/StaticPages/LoginPage";
 import RegisterPage from "./components/StaticPages/RegisterPage";
 import ViewBooks from "./components/Books/ViewBooks";
@@ -43,8 +38,6 @@ class App extends Component {
             {
               //Private Routes
             }
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addPerson" component={AddPerson} />
             <Route exact path="/adminlanding" component={AdminLanding} />
             <Route exact path="/pendingpublishers" component={PendingBusinessApps} />
            
